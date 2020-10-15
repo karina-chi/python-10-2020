bb63029c4fb2716a52a886ba4b309b1a193674d9
karina-chi <72555973+karina-chi@users.noreply.github.com>
2020-10-15 20:33:18 +0300

    Update task1.py

--- Lesson3/task1.py
+++ Lesson3/task1.py
@@ -2,12 +2,14 @@
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
 def my_division(arg_1, arg_2):
-    return arg_1 / arg_2
+    try:
+        return arg_1 / arg_2
+    except ZeroDivisionError:
+        print("На ноль делить нельзя")
+        exit()
 
+        
 var_1 = int(input("Введите делимое: "))
 var_2 = int(input("Введите делитель: "))
 
-try:
-    print(f'{var_1} : {var_2} = {my_division(var_1, var_2)}')
-except ZeroDivisionError:
-    print("На ноль делить нельзя")
+print(f'{var_1} : {var_2} = {my_division(var_1, var_2)}')
