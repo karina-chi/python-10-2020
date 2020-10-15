4e2fa7d7f041e41e2e66978614fcd23df3b7223c
karina-chi <72555973+karina-chi@users.noreply.github.com>
2020-10-15 20:57:32 +0300

    Update task3.py

--- Lesson3/task3.py
+++ Lesson3/task3.py
@@ -6,5 +6,6 @@ def my_func(arg_1, arg_2, arg_3):
     new_list.remove(min(arg_1, arg_2, arg_3))
     return sum(new_list)
 
+a, b, c = int(input("Введите первое число: ")), int(input("Введите второе число: ")), int(input("Введите третье число: "))
 
-print(my_func(7, 5, 4))
+print(my_func(a, b, c))
